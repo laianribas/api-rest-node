8c94d6eb2462a20ec1e1f44ecc9a223cca1ea752
laianribas <laian67a@gmail.com>
2022-03-25 19:39:02 -0300

    dotenv

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,14 @@
 import { Sequelize } from 'sequelize'
+import 'dotenv/config'
 
-const connection = new Sequelize('market2', 'root', '5121056', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
+const connection = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS, {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    }
+)
 
 try {
     connection.authenticate()
